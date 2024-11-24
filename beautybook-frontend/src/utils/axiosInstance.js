bfff4f880d70540856b66baeb387a75b650e2fc6
Yousef Alhindawi <eng.yousefalhindawi@gmail.com>
2024-11-24 12:38:35 +0300

    fix the axiosInstance interceptors response error

--- beautybook-frontend/src/utils/axiosInstance.js
+++ beautybook-frontend/src/utils/axiosInstance.js
@@ -20,12 +20,18 @@ axiosInstance.interceptors.request.use(
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401 || error.response?.status === 403) {
+    // unless it's not /login route and not /register route and status is 401 or 403  then redirect to login  page
+    if (
+      (error.response?.status === 401 || error.response?.status === 403) &&
+      !window.location.href.includes("/login") &&
+      !window.location.href.includes("/register")
+    ) {
       // redirect to login if token is invalid
       console.log("Token is invalid");
       localStorage.removeItem("token");
       window.location.href = "/login";
     }
+    // but if it's /login route then show error message
     return Promise.reject(error);
   }
 );
