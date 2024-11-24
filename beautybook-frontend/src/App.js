118c3486325f3e293757f19096b8d7bcc7524afd
Yousef Alhindawi <eng.yousefalhindawi@gmail.com>
2024-11-24 15:22:01 +0300

    ErrorBoundary & lazy loeading

--- beautybook-frontend/src/App.js
+++ beautybook-frontend/src/App.js
@@ -1,41 +1,80 @@
-import React from "react";
+import React, { Suspense } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-  // Navigate,
 } from "react-router-dom";
-// import Login from "./components/Auth/Login";
-// import Register from "./components/Auth/Register";
-// import AppointmentList from "./components/Appointments/AppointmentList";
-// import ProtectedRoute from "./components/Shared/ProtectedRoute";
-// import Test from "../Test";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
-// import Test from "./Test";
 import ProtectedRoute from "./components/Shared/ProtectedRoute";
 import AppNavbar from "./components/Layout/Navbar";
-import Staffs from "./pages/Staffs";
-import Services from "./pages/Services";
+// import Staffs from "./pages/Staffs";
+// import Services from "./pages/Services";
 import Appointments from "./pages/Appointments";
 import AppFooter from "./components/Layout/Footer";
-
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
+import { Spinner } from "react-bootstrap";
+const LazyStaffs = React.lazy(() => import("./pages/Staffs"));
+const LazyServices = React.lazy(() => import("./pages/Services"));
 const App = () => {
   return (
-    <Router>
-      <AppNavbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<Navigate to="/appointments" />} />
-          <Route path="/appointments" element={<Appointments />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/staffs" element={<Staffs />} />
-        </Route>
-        {/* <Route path="/" element={<Navigate to="/appointments" />} /> */}
-        {/* <Route
+    <ErrorBoundary fallback={<div>Something went wrong!</div>}>
+      <Router>
+        <AppNavbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Navigate to="/appointments" />} />
+            <Route path="/appointments" element={<Appointments />} />
+            <Route
+              path="/services"
+              element={
+                <Suspense
+                  fallback={
+                    <div
+                      className="position-relative"
+                      style={{
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        minHeight: "50vh",
+                      }}
+                    >
+                      <Spinner animation="border" variant="primary" />
+                    </div>
+                  }
+                >
+                  <LazyServices />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/staffs"
+              element={
+                <Suspense
+                  fallback={
+                    <div
+                      className="position-relative"
+                      style={{
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        minHeight: "50vh",
+                      }}
+                    >
+                      <Spinner animation="border" variant="primary" />
+                    </div>
+                  }
+                >
+                  <LazyStaffs />
+                </Suspense>
+              }
+            />
+          </Route>
+          {/* <Route path="/" element={<Navigate to="/appointments" />} /> */}
+          {/* <Route
           path="/appointments"
           element={
             <ProtectedRoute>
@@ -43,9 +82,10 @@ const App = () => {
             </ProtectedRoute>
           }
         /> */}
-      </Routes>
-      <AppFooter />
-    </Router>
+        </Routes>
+        <AppFooter />
+      </Router>
+    </ErrorBoundary>
   );
 };
 
